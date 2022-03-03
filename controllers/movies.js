24906ac16b1f652cac8a0491e63eb3e4d7825983
Vladimir <vladimirkrylov@gmail.com>
2022-03-03 18:43:44 +0300

    add: controller для user,movie

---
+++ controllers/movies.js
@@ -0,0 +1,76 @@
+const Movie = require('../models/movie');
+
+const Forbidden = require('../errors/forbidden');
+const NotFoundError = require('../errors/not-found');
+const BadRequestError = require('../errors/bad-request');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({ owner: req.user._id })
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then((newMovie) => {
+      res.status(200).send(newMovie);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new BadRequestError('Переданы некорректные данные');
+      } else {
+        next(err);
+      }
+    })
+    .catch(next);
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  const id = req.user._id;
+  Movie.findById(req.params._id)
+    .then((movie) => {
+      if (!movie) {
+        throw new NotFoundError('Нет фильма с таким id');
+      }
+      if (movie.owner.toString() !== id) {
+        throw new Forbidden('Нет доступа для удаления фильма');
+      } else {
+        return movie.remove()
+          .then(() => res.send({ message: 'deleted' }));
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        throw new BadRequestError('Переданы некорректные данные');
+      } else {
+        next(err);
+      }
+    })
+    .catch(next);
+};
